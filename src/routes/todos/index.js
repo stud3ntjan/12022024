b733b214644fece613d0a3fc8978d3c7cdde40ce
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-02-15 13:56:25 +0100

    create get & post

--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -14,6 +14,11 @@ TodosRouter.get("/byid", (req, res) => {
   res.status(StatusCodes.OK).send("Get Todo by id");
 });
 
+// Get all todos
+TodosRouter.get("/all", (req, res) => {
+  res.status(StatusCodes.OK).send("Get all Todos");
+});
+
 TodosRouter.post("/byuserid", (req, res) => {
   res.status(StatusCodes.OK).send("Get Todo by user id");
 });
@@ -29,6 +34,11 @@ TodosRouter.put("/update", (req, res) => {
 // POST REQUESTS
 TodosRouter.post("/create", (req, res) => {
   res.status(StatusCodes.CREATED).send("Erstellen eines Todos");
+});
+
+// Create a new todo
+TodosRouter.post("/todo", (req, res) => {
+  res.status(StatusCodes.CREATED).send("Neues Todo wurde erstellt");
 });
 
 // DELETE REQUEST
